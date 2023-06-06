9357f7d547af4eed578f8fcc25ad815c43e395a3
Ajith <ajithpkumar231@gmail.com>
2023-06-06 10:06:20 +0530

    Basic functionalities like login, logout, add/remoce from cart etc done

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import "./Cart.css";
-import { cartActions } from "./../store/cartSlice";
+import { cartActions } from "./../store/cart-slice";
 const CartItem = ({ name, quantity, total, price, id }) => {
+  console.log(
+    "inside cart item " + name + " " + quantity + " " + total + " " + price
+  );
+
   const dispatch = useDispatch();
+
   const removeHandler = () => {
     dispatch(cartActions.removeFromCart(id));
   };
+
   const addHandler = () => {
     dispatch(
       cartActions.addToCart({
